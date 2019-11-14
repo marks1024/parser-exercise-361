752e7112e21d985e6140c343c7a1fc755838fbf6
marks <marks1024@users.noreply.github.com>
2019-11-14 16:02:19 +0600

    edits

--- src/test/java/kz/edu/nu/cs/se/ParserTest.java
+++ src/test/java/kz/edu/nu/cs/se/ParserTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-// import kz.edu.nu.cs.se.solve.MyLanguageParser;
+import kz.edu.nu.cs.se.solve.MyLanguageParser;
 
 public class ParserTest {
     
